52e6edd5a2918f323d89992f4bc6c4baf6e3211a
Koustabh Goswami <koustabhgoswami12345@gmail.com>
2023-07-26 23:37:29 +0530

    first commit:creates the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000; //by default port is 80
+app.listen(port, function(err) {
+    if (err) {
+        // console.log('Error listening to port')
+        //inter-polation
+        console.log(`Error listening to port: ${err}`);
+    }
+    console.log(`Server is running on port:${port}`);
+});
