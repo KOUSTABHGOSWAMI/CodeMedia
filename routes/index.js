a25783cda26e8c7b33e6d714f02c0c452e24f62e
Koustabh Goswami <koustabhgoswami12345@gmail.com>
2023-07-27 23:49:47 +0530

    added extra route and controller file

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
-console.log('Router is loaded');
+router.use('/users', require('./users'));
+// from any further routes access from here
+//router.use('/routerName',require('./routerfile'));
+// console.log('Router is loaded');
 module.exports = router;
