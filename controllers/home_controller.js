621208023df2e2163640e09e95d9c18fac72e97c
Koustabh Goswami <koustabhgoswami12345@gmail.com>
2023-07-29 01:27:46 +0530

    added header footers and implemented partials

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req, res) {
-    res.end('<h1> The controller is up and running for codeial </h1>');
+    // res.end('<h1> The controller is up and running for codeial </h1>');
+    return res.render('home', {
+        title: "Home"
+    });
 }
