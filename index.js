88a7e94c0e1e4968a0bf5e6834cc3a37bbcd23c7
Koustabh Goswami <koustabhgoswami12345@gmail.com>
2023-07-30 01:26:48 +0530

    setup static file for layouts and pages

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const port = 8000; //by default port is 80
 //use express router
+
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+//including the express layout 
+
+app.use(express.static('./assets')); //including the assets folder where the files can be accessed
 app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 app.listen(port, function(err) {
     if (err) {
         // console.log('Error listening to port')
