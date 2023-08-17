e54a2621251bac981b39af26367ebed435f0bff5
Koustabh Goswami <koustabhgoswami12345@gmail.com>
2023-08-18 00:26:47 +0530

    added checks regarding posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const postController = require('../controllers/posts_controller');
 
-router.post('/create', postController.create);
+router.post('/create', passport.checkAuthentication, postController.create);
 module.exports = router;
